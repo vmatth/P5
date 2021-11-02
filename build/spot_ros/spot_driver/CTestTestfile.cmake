c9eb3324b83405432476cf376861d13fce98e1e7
vmatth <vmatth19@student.aau.dk>
2021-11-02 09:25:14 +0100

    untrack build folder

--- build/spot_ros/spot_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/axel/P5/src/spot_ros/spot_driver
-# Build directory: /home/axel/P5/build/spot_ros/spot_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
