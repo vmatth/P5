3984601e74effbbf24b40cf959831d482bf15068
Simon Sunesen Gaasdal <sgaasd19@student.aau.dk>
2021-11-08 15:22:41 +0100

    added test data_persons

---
+++ pose_estimation/test_persons4.py
@@ -0,0 +1,12 @@
+import math
+
+person1 = [5,5.5,math.pi/2]
+person2 = [4,5,math.pi]
+person3 = [6,4,math.pi/2]
+person4 = [1,8,0]
+
+array = [person1, person2, person3, person4]
+print(array)
+
+
+print(array[0][0])
