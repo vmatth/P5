71c74d87e07de87bfc8e955622f8aebc0c8cc344
Simon Sunesen Gaasdal <sgaasd19@student.aau.dk>
2021-11-08 15:29:33 +0100

    direction add

--- pose_estimation/test_persons4.py
+++ pose_estimation/test_persons4.py
@@ -1,9 +1,12 @@
 import math
 
-person1 = [5,5.5,math.pi/2]
-person2 = [4,5,math.pi]
-person3 = [6,4,math.pi/2]
-person4 = [1,8,0]
+
+pi = math.pi
+
+person1 = [5,5.5, pi - pi/4]
+person2 = [4,5,pi/4]
+person3 = [6,4,pi + pi/4]
+person4 = [1,8,3*pi/2]
 
 array = [person1, person2, person3, person4]
 print(array)
