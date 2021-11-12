a9054f21d653b6b8c7a69dd1d8002fbd54334bf0
Zatrokk <jfalk19@student.aau.dk>
2021-11-12 13:18:14 +0100

    Ray casting Done, Contruct_opsace only needs clean
    Co-authored-by: Simon Sunesen Gaasdal <sgaasd@users.noreply.github.com>

--- pose_estimation/Construct_Ospaces.py
+++ pose_estimation/Construct_Ospaces.py
@@ -2,35 +2,63 @@ import numpy as np
 from scipy.spatial import ConvexHull, convex_hull_plot_2d
 import matplotlib.pyplot as plt
 import random
-center1 = [0,0]
-center2 = [3,3]
-center3 = [2,1.5]
-center4 = [3,1]
-center5 = [0,4]
+import generate_OspaceCombinations
 
+center1 = [0,0] # 0,0
+center2 = [3,3] # 3,3
+center3 = [2,1.5] # 2,1.5
+center4 = [3,1] # 3,1
+center5 = [0,4] # 0,4
 
-center = [center1, center2, center3, center4, center5]
-
+center = [center1, center2, center3]
 
 
 arr = np.array([center]).reshape(-1, 2)
 #arr = np.array(list(zip(x, y)))
-print("right:", arr)
+#print("right:", arr)
 
 def convexHull(peopleCentrum):
-
+    combinations = generate_OspaceCombinations.oSpaceCombinations(len(peopleCentrum))
+    #print("comb: ",combinations)
     array=[]
+    arr=[]
+    returnList = []
     array=peopleCentrum
+    tempArray = []
+    tempArrayPeople = []
+    for i in range(0,len(combinations)):
+        tempArrayPeople = []
+        tempArray = []
+        for j in range(0, len(combinations[i])):
+        #print(combinations[0][0])
+            tempArrayPeople.append(array[(combinations[i][j])-1])
+            tempNpArrayPeople = np.array(tempArrayPeople)
+ #   print('Rigtig array: ', array)
+ #   print('HIT!: ', tempArrayPeople)
+            plt.plot(array[:,0], array[:,1], '+')
+        #print("temp", tempNpArrayPeople, type(tempNpArrayPeople))
+        #print("array", array, type(array))
+        if len(tempNpArrayPeople) > 2:
+            hull = ConvexHull(tempNpArrayPeople)
+            for simplex in hull.simplices:
+                #plt.plot(tempArrayPeople[simplex, 0], tempArrayPeople[simplex, 1], 'k-')
+                #print("shity", tempNpArrayPeople, type(tempNpArrayPeople))
+                #print("x",tempNpArrayPeople[simplex,0])
+                #print("y",tempNpArrayPeople[simplex,1])
+                arr = np.array([[tempNpArrayPeople[simplex[0], 0], tempNpArrayPeople[simplex[0], 1]],[tempNpArrayPeople[simplex[1], 0],tempNpArrayPeople[simplex[1], 1]]]).reshape(-1, 2)
+                tempArray.append(arr)
+        elif len(tempNpArrayPeople) == 2:
+            tempArray.append(tempNpArrayPeople)
+#            print('TMEPOREP: ', tempNpArrayPeople) 
+        returnList.append(tempArray)
+    print('array1: ', returnList[1])
+    if len(tempArray) < len(tempNpArrayPeople):
+        print('The polygon is not valid aw :(')
+        return 0
+    else:
+        return returnList
+   # plt.show()
 
-    plt.plot(array[:,0], array[:,1], '+')
-    hull = ConvexHull(array)
-    for simplex in hull.simplices:
-        plt.plot(array[simplex, 0], array[simplex, 1], 'k-')
-        print("x",array[simplex,0])
-        print("y",array[simplex,1])
-    plt.show()
-
-    if 2+2==4:
-        print("simon er gay")
+#Test om inputlængde og output længde er det samme
 
 convexHull(arr)
