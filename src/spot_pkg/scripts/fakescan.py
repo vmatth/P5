f5e76a601120c173ff08056f413f7521f2bf6156
vmatth <vmatth19@student.aau.dk>
2021-11-30 15:17:35 +0100

    fakescan

--- src/spot_pkg/scripts/fakescan.py
+++ src/spot_pkg/scripts/fakescan.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from ctypes import resize
 import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Header
@@ -23,21 +24,32 @@ if __name__ == '__main__':
         h.stamp = rospy.Time.now()
         h.frame_id = "sick_tim"
 
+        num_readings = 100
+        laser_frequency = 40
+        ranges = []
+        intensities = []
+        count = 0
+
+        i = 0
+
+        while(i < num_readings):
+            ranges.append(1.5 + i/100)
+            intensities.append(4+count)
+            i = i + 1
+
         laser = LaserScan()
         laser.header = h
-        laser.angle_min = 0
-        laser.angle_max = 2 * math.pi
+        laser.angle_min = -1.57
+        laser.angle_max = 1.57
 
-        laser.angle_increment = (2 * math.pi / 10)
-
-        laser.time_increment = 0
-
-        laser.scan_time = 0
+        laser.angle_increment = (3.14 / num_readings)
+        laser.time_increment = (1/laser_frequency) / (num_readings)
 
         laser.range_min = 0
-        laser.range_min = 30
+        laser.range_min = 4
 
-        laser.ranges = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+        laser.ranges = ranges
+        laser.intensities = intensities
 
 
         pub.publish(laser)
