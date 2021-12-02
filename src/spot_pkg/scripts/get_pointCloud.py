aa701a199a5f54d321309244e52dd2d5766f039b
vmatth <vmatth19@student.aau.dk>
2021-12-02 11:14:59 +0100

    Cleanup for the boss

--- src/spot_pkg/scripts/get_pointCloud.py
+++
@@ -1,6 +0,0 @@
-#import rospy
-
-print('http://wiki.ros.org/realsense2_camera')
-print('https://github.com/IntelRealSense/realsense-ros')
-print('http://docs.ros.org/en/noetic/api/sensor_msgs/html/msg/PointCloud.html')
-
