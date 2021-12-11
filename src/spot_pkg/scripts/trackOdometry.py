68b85ea88c79c4f3a4bfe3ef4110d99d39686f45
vmatth <vmatth@student.aau.dk>
2021-12-11 16:09:20 +0100

    Spot tracker

--- src/spot_pkg/scripts/trackOdometry.py
+++ src/spot_pkg/scripts/trackOdometry.py
@@ -8,28 +8,31 @@ class Tracker:
     def __init__(self):
         self.vis_pub = rospy.Publisher('markers', Marker, queue_size=10)
         self.logPosition = []
-        rospy.Subscriber("/odom", Odometry, self.callback)
+        rospy.Subscriber("/spot/odometry", Odometry, self.callback)
+	self.id = 0
 
     def callback(self, data):
         rospy.loginfo("Odom from spot: %s", data)
-        self.logPosition.append(data.pose.position)
+        self.logPosition.append(data.pose.pose.position)
 
         with open('odom_log', 'w') as f:
             writer = csv.writer(f)
-            writer.writerow(data.pose.position)
-
+            #writer.writerow(data.pose.pose.position)
+	self.id = self.id + 1
         marker = Marker()
-        marker.header.frame_id = "/body"
+        marker.header.frame_id = "gpe"
         marker.type = marker.SPHERE
         marker.action = marker.ADD
-        marker.scale.x = 0.05
-        marker.scale.y = 0.05
-        marker.scale.z = 0.05
+	marker.id = self.id
+	marker.frame_locked = True
+        marker.scale.x = 0.1
+        marker.scale.y = 0.1
+        marker.scale.z = 0.1
         marker.color.a = 1.0
         marker.pose.orientation.w = 1.0
-        marker.pose.position.x = data.pose.position.x
-        marker.pose.position.y = data.pose.position.y
-        marker.pose.position.z = data.pose.position.z
+        marker.pose.position.x = 0#data.pose.pose.position.x
+        marker.pose.position.y = 0#data.pose.pose.position.y
+        marker.pose.position.z = 0#data.pose.pose.position.z
 
         self.vis_pub.publish(marker)
 
